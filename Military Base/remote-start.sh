6a08359177bf572b7a4c5f26ea4deec88e6a47cd
Weili Bai <157757005+Weili-Bai@users.noreply.github.com>
2025-05-27 19:33:58 -0700

    Update remote-start.sh

--- Military Base/remote-start.sh
+++ Military Base/remote-start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Set Oracle environment
-if [ -d /opt/oracle/instantclient_19_8 ]; then
+if [ -d /opt/oracle/instantclient_23_7 ]; then
+    export ORACLE_HOME=/opt/oracle/instantclient_23_7
+    export LD_LIBRARY_PATH=$ORACLE_HOME
+elif [ -d /opt/oracle/instantclient_19_8 ]; then
     export ORACLE_HOME=/opt/oracle/instantclient_19_8
     export LD_LIBRARY_PATH=$ORACLE_HOME
 elif [ -d /usr/lib/oracle/19.6/client64/lib ]; then
